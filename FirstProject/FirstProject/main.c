d4cbe4cbe32236109441ac5297dc56fa49cd021d
Beela <bla@senia.dk>
2022-05-17 11:05:24 +0200

    first project

---
+++ FirstProject/FirstProject/main.c
@@ -0,0 +1,18 @@
+/*
+ * FirstProject.c
+ *
+ * Created: 17-05-2022 11:04:55
+ * Author : bla
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
