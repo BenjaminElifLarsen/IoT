efead5433f4d22d8e926abce9db90ed79a0411d7
Beela <bla@senia.dk>
2022-06-03 10:38:52 +0200

    minor improvements

--- DebuggerTest/DebuggerTest/main.c
+++ DebuggerTest/DebuggerTest/main.c
@@ -8,24 +8,18 @@
 #include <avr/io.h>
 
 void bitFlip(uint8_t *port, uint8_t bit){
+	uint8_t *ddr = port;	
+	*ddr--;
+	*ddr ^= (1 << bit);
 	*port ^= (1 << bit);	
 }
 
 
 int main(void) //Using JTAG
-{
-	uint8_t *ddr = 0x24;
-	*ddr |= (1 << 2);
-	*ddr |= (1 << 1);
-	*ddr |= (1 << 0);
-	
-	uint8_t pos = 0;
-	
+{	
 	uint8_t *port = 0x25;	
-	*port |= (1 << 2);
-	*port |= (1 << 1);
-	*port |= (1 << 0);
-    while (1) // Reason for no delay is that it is only meant to be run through debugging using JTAG.
+	uint8_t pos = 0;	
+    while (1) // Reason for no delay is that it is only meant to be run through debugging using JTAG with a breakpoint in the loop.
     {
 		bitFlip(port,pos);
 		pos = (++pos) % 3; 
